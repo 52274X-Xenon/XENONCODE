bf9c77105289ae3c5b38ae5f8683b906e798acd9
52274X-Xenon <hridhay.ramesh@gmail.com>
2024-12-08 21:55:19 -0800

    first commit

---
+++ include/subsystems.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "api.h"
+
+// Your motors, sensors, etc. should go here.  Below are examples
+
+// inline pros::Motor intake(1);
+// inline pros::adi::DigitalIn limit_switch('A');
+
+inline pros::Motor intake(5);
+inline pros::Motor ladybrown(6);
+inline pros::Rotation ldbrotation(7);
